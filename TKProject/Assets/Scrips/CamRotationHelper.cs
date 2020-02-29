a47031a598dc7113033c7e8dcf6fddeae6ca6f34
PammerJoe <pammerka@gmail.com>
2020-02-29 16:38:57 +0100

    Add Project Base
    Base project with the basic setup made.

---
+++ TKProject/Assets/Scrips/CamRotationHelper.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamRotationHelper : MonoBehaviour
+{
+
+    private Quaternion lookRotationVar;
+    public GameObject cameraHolder;
+    // Update is called once per frame
+    void Update()
+    {
+        lookRotationVar = lookRotationVar * Quaternion.Inverse(this.transform.rotation);
+        lookRotationVar = lookRotationVar * cameraHolder.transform.rotation;
+        Vector3 newRotation = lookRotationVar.eulerAngles;
+        cameraHolder.transform.localEulerAngles = newRotation;
+    }
+}
