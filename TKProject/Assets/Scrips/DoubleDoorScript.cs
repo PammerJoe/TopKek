7fa95eaacddb3e1ed0b5636477a402e81d101c4b
PammerJoe <pammerka@gmail.com>
2020-04-22 15:51:49 +0200

    Ajto, lampa
    Zseblampa elekszitve
    Ajto nyitas, zaras animacio es funkció
    Shaderek

---
+++ TKProject/Assets/Scrips/DoubleDoorScript.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoubleDoorScript : MonoBehaviour
+{
+
+    private Animator _animator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _animator = GetComponent<Animator>();
+    }
+
+    void OnTriggerStay(Collider other)
+    {
+        
+        if (other.tag == "MainCamera")
+        {
+            
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                if (_animator.GetBool("Opened"))
+                {
+                    _animator.SetBool("Opened", false);
+                }
+                else
+                {
+                    _animator.SetBool("Opened", true);
+                }
+            }
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
